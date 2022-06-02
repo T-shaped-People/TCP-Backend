073b2e7c4abd9cb2cee792a65a54422c8ef19a6c
min050410 <min050410@gmail.com>
2022-06-02 12:55:16 +0000

    fix: client 배열 추가, socket명 변경

--- src/drawing/drawing.gateway.ts
+++ src/drawing/drawing.gateway.ts
@@ -11,15 +11,21 @@ import { Server, Socket } from 'socket.io';
 // import { Logger } from '@nestjs/common';
 
 type Position = {
-    x: number
-    y: number
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    color: string
 }
 
 @WebSocketGateway({ cors: true })
 export class DrawingGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
-    async handleConnection(): Promise<void> {
+    drClients=[];
+
+    async handleConnection(client: any): Promise<void> {
         this.users++;
+        this.drClients.push(client);
         this.server.emit('users', this.users);
     }
 
@@ -30,10 +36,10 @@ export class DrawingGateway implements OnGatewayConnection, OnGatewayDisconnect 
 
     @WebSocketServer() server: any;
     users: number = 0;
-
-    @SubscribeMessage('position')
-    async onPosition(@MessageBody() data: Position): Promise<void> {
-        this.server.emit('sposition', data);
-    }
     
+    @SubscribeMessage('draw')
+    async onPosition(client: any, data: Position): Promise<void> {
+        client.broadcast.emit('draw', data);
+        // this.server.emit('sposition', data);
+    }
 }
