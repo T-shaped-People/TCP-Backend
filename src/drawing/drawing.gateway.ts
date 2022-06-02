085f8e0935b4b3fd89175f615109dcd813708db5
min050410 <min050410@gmail.com>
2022-06-02 13:20:40 +0000

    add: 주석추가, 클라이언트 페이로드 변경

--- src/drawing/drawing.gateway.ts
+++ src/drawing/drawing.gateway.ts
@@ -8,8 +8,9 @@ import {
 } from '@nestjs/websockets';
 
 import { Server, Socket } from 'socket.io';
-// import { Logger } from '@nestjs/common';
 
+
+// payload
 type Position = {
     x1: number,
     y1: number,
@@ -21,14 +22,17 @@ type Position = {
 @WebSocketGateway({ cors: true })
 export class DrawingGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
+    // 클라이언트 배열
     drClients=[];
 
+    // 소켓 통신 시작
     async handleConnection(client: any): Promise<void> {
         this.users++;
         this.drClients.push(client);
         this.server.emit('users', this.users);
     }
 
+    // 소켓 통신 해제
     async handleDisconnect(): Promise<void> {
         this.users--;
         this.server.emit('users', this.users);
@@ -39,7 +43,7 @@ export class DrawingGateway implements OnGatewayConnection, OnGatewayDisconnect 
     
     @SubscribeMessage('draw')
     async onPosition(client: any, data: Position): Promise<void> {
+        // broadcast로 자신이 아닌 다른 클라이언트에게 메세지
         client.broadcast.emit('draw', data);
-        // this.server.emit('sposition', data);
     }
 }
