f7fced17a22b60360ba914fcc74ac0823bba4948
이현준 <80656849+leehj050211@users.noreply.github.com>
2022-06-05 12:59:36 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,13 @@ import { AuthModule } from 'src/auth/auth.module';
 import { BoardModule } from 'src/board/board.module';
 import { TeamModule } from 'src/team/team.module';
 import { UserModule } from './user/user.module';
-import { DrawingController } from './drawing/drawing.controller';
 import { DrawingModule } from './drawing/drawing.module';
-import { ChatController } from './chat/chat.controller';
-import { ChatGateway } from './chat.gateway';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mariadb',
-      // port: 3306,
       host: process.env.DB_HOST,
       username: process.env.DB_USER,
       password: process.env.DB_PW,
@@ -30,7 +26,7 @@ import { ChatModule } from './chat/chat.module';
     TeamModule,
     DrawingModule,
     ChatModule],
-  controllers: [ChatController],
-  providers: [ChatGateway],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
