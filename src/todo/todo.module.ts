fdc513fa5550f67419c962016c72ef3cc87c2441
youngmin <min050410@gmail.com>
2022-08-25 17:31:11 +0900

    Add: Todo Entity 정의

---
+++ src/todo/todo.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
+
+@Module({
+  controllers: [TodoController],
+  providers: [TodoService]
+})
+export class TodoModule {
+    
+}
